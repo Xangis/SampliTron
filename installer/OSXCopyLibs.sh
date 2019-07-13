1ef3963ecd071fafd9eabd1e715a4d95cc17c48c
Jason Champion <xangis@Jasons-MBP.domain>
2019-07-13 10:03:05 -0700

    Add library copy/path utils, update library path script. App builds and runs now.

---
+++ installer/OSXCopyLibs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This should only need to be run once to get the wxWidgets libraries into the app directory.
+mkdir -p Samplitron.app/Contents/Frameworks
+cp /usr/local/lib/libwx_osx_cocoau_adv-3.1.dylib Samplitron.app/Contents/Frameworks/
+cp /usr/local/lib/libwx_osx_cocoau_core-3.1.dylib Samplitron.app/Contents/Frameworks/
+cp /usr/local/lib/libwx_osx_cocoau_html-3.1.dylib Samplitron.app/Contents/Frameworks/
+cp /usr/local/lib/libwx_baseu_net-3.1.dylib Samplitron.app/Contents/Frameworks/
+cp /usr/local/lib/libwx_baseu_net-3.1.dylib Samplitron.app/Contents/Frameworks/
+cp /usr/local/lib/libwx_osx_cocoau_qa-3.1.dylib Samplitron.app/Contents/Frameworks/
+cp /usr/local/lib/libwx_baseu_xml-3.1.dylib Samplitron.app/Contents/Frameworks/
+cp /usr/local/lib/libwx_osx_cocoau_xrc-3.1.dylib Samplitron.app/Contents/Frameworks/
+cp /usr/local/lib/libwx_baseu-3.1.dylib Samplitron.app/Contents/Frameworks/
+cp ../../../lib/SDL2_ttf-2.0.15/.libs/libSDL2_ttf-2.0.0.dylib Samplitron.app/Contents/Frameworks/
+cp ../../..//lib/rtmidi-4.0.0/.libs/librtmidi.5.dylib Samplitron.app/Contents/Frameworks/
--- installer/OSXLibraryPaths.sh
+++ installer/OSXLibraryPaths.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 install_name_tool -change /usr/local/lib/libsndfile.1.dylib @executable_path/../Frameworks/libsndfile.1.dylib Samplitron.app/Contents/MacOS/SampliTron
 install_name_tool -change /usr/local/lib/libportaudio.2.dylib @executable_path/../Frameworks/libportaudio.2.dylib Samplitron.app/Contents/MacOS/SampliTron
+install_name_tool -change /usr/local/lib/libfreetype.6.dylib @executable_path/../Frameworks/libfreetype.6.dylib Samplitron.app/Contents/MacOS/SampliTron
 install_name_tool -change /usr/local/lib/libwx_osx_cocoau_xrc-3.1.dylib @executable_path/../Frameworks/libwx_osx_cocoau_xrc-3.1.dylib Samplitron.app/Contents/MacOS/SampliTron
 install_name_tool -change /usr/local/lib/libwx_osx_cocoau_html-3.1.dylib @executable_path/../Frameworks/libwx_osx_cocoau_html-3.1.dylib Samplitron.app/Contents/MacOS/SampliTron
 install_name_tool -change /usr/local/lib/libwx_osx_cocoau_qa-3.1.dylib @executable_path/../Frameworks/libwx_osx_cocoau_qa-3.1.dylib Samplitron.app/Contents/MacOS/SampliTron
@@ -10,19 +11,21 @@ install_name_tool -change /usr/local/lib/libwx_baseu_xml-3.1.dylib @executable_p
 install_name_tool -change /usr/local/lib/libwx_baseu_net-3.1.dylib @executable_path/../Frameworks/libwx_baseu_net-3.1.dylib Samplitron.app/Contents/MacOS/SampliTron
 install_name_tool -change /usr/local/lib/libwx_baseu-3.1.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib Samplitron.app/Contents/MacOS/SampliTron
 # wxWidget library references to other wxWidgets libraries.
-install_name_tool -change /usr/local/lib/libwx_osx_cocoau_core-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_core-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_adv-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_adv-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_core-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_osx_cocoau_core-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_core-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_html-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_html-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_baseu_net-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_osx_cocoau_core-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_core-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_qa-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_qa-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu_xml-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu_xml-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_qa-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_baseu_xml-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_osx_cocoau_core-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_core-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_baseu_xml-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_baseu_xml-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_osx_cocoau_html-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_html-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
-install_name_tool -change /usr/local/lib/libwx_osx_cocoau_adv-3.1.0.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_adv-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_osx_cocoau_core-3.1.2.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_core-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_adv-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu-3.1.2.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_adv-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu-3.1.2.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_core-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_osx_cocoau_core-3.1.2.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_core-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_html-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu-3.1.2.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_html-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu-3.1.2.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_baseu_net-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_osx_cocoau_core-3.1.2.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_core-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_qa-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu-3.1.2.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_qa-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu_xml-3.1.2.0.0.dylib @executable_path/../Frameworks/libwx_baseu_xml-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_qa-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu-3.1.2.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_baseu_xml-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_osx_cocoau_core-3.1.2.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_core-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu-3.1.2.0.0.dylib @executable_path/../Frameworks/libwx_baseu-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_baseu_xml-3.1.2.0.0.dylib @executable_path/../Frameworks/libwx_baseu_xml-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_osx_cocoau_html-3.1.2.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_html-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
+install_name_tool -change /usr/local/lib/libwx_osx_cocoau_adv-3.1.2.0.0.dylib @executable_path/../Frameworks/libwx_osx_cocoau_adv-3.1.dylib Samplitron.app/Contents/Frameworks/libwx_osx_cocoau_xrc-3.1.dylib
+install_name_tool -change /usr/local/lib/libSDL2-2.0.0.dylib @executable_path/../Frameworks/libSDL2-2.0.0.dylib Samplitron.app/Contents/Frameworks/libSDL2_ttf-2.0.0.dylib
+install_name_tool -change /usr/local/lib/libfreetype.6.dylib @executable_path/../Frameworks/libfreetype.6.dylib Samplitron.app/Contents/Frameworks/libSDL2_ttf-2.0.0.dylib
 otool -L Samplitron.app/Contents/MacOS/SampliTron
